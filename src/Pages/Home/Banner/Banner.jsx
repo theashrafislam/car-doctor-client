284b0140a21c40ee339c4a02d797fd8d3264050b
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-05-01 01:24:50 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,116 @@
+import img1 from '../../../assets/images/banner/1.jpg';
+import img2 from '../../../assets/images/banner/2.jpg';
+import img3 from '../../../assets/images/banner/3.jpg';
+import img4 from '../../../assets/images/banner/4.jpg';
+import img5 from '../../../assets/images/banner/5.jpg';
+import img6 from '../../../assets/images/banner/6.jpg';
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[500px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full" />
+                <div className="absolute h-full flex items-center left-0 top-0 text-white bg-gradient-to-r from-gray-900 to-transparent">
+                    <div className='text-white space-y-6 w-1/3 pl-16'>
+                        <h1 className='text-5xl font-bold'>Affordable Price For Car Servicing</h1>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className='flex items-center gap-6'>
+                            <button className="btn btn-outline btn-secondary">Discover More</button>
+                            <button className="btn btn-active btn-primary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex gap-4 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={img2} className="w-full" />
+                <div className="absolute h-full flex items-center left-0 top-0 text-white bg-gradient-to-r from-gray-900 to-transparent">
+                    <div className='text-white space-y-6 w-1/3 pl-16'>
+                        <h1 className='text-5xl font-bold'>Affordable Price For Car Servicing</h1>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className='flex items-center gap-6'>
+                            <button className="btn btn-outline btn-secondary">Discover More</button>
+                            <button className="btn btn-active btn-primary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex gap-4 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={img3} className="w-full" />
+                <div className="absolute h-full flex items-center left-0 top-0 text-white bg-gradient-to-r from-gray-900 to-transparent">
+                    <div className='text-white space-y-6 w-1/3 pl-16'>
+                        <h1 className='text-5xl font-bold'>Affordable Price For Car Servicing</h1>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className='flex items-center gap-6'>
+                            <button className="btn btn-outline btn-secondary">Discover More</button>
+                            <button className="btn btn-active btn-primary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex gap-4 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img4} className="w-full" />
+                <div className="absolute h-full flex items-center left-0 top-0 text-white bg-gradient-to-r from-gray-900 to-transparent">
+                    <div className='text-white space-y-6 w-1/3 pl-16'>
+                        <h1 className='text-5xl font-bold'>Affordable Price For Car Servicing</h1>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className='flex items-center gap-6'>
+                            <button className="btn btn-outline btn-secondary">Discover More</button>
+                            <button className="btn btn-active btn-primary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex gap-4 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide5" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide5" className="carousel-item relative w-full">
+                <img src={img5} className="w-full" />
+                <div className="absolute h-full flex items-center left-0 top-0 text-white bg-gradient-to-r from-gray-900 to-transparent">
+                    <div className='text-white space-y-6 w-1/3 pl-16'>
+                        <h1 className='text-5xl font-bold'>Affordable Price For Car Servicing</h1>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className='flex items-center gap-6'>
+                            <button className="btn btn-outline btn-secondary">Discover More</button>
+                            <button className="btn btn-active btn-primary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex gap-4 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide5" className="btn btn-circle">❮</a>
+                    <a href="#slide6" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide6" className="carousel-item relative w-full">
+                <img src={img6} className="w-full" />
+                <div className="absolute h-full flex items-center left-0 top-0 text-white bg-gradient-to-r from-gray-900 to-transparent">
+                    <div className='text-white space-y-6 w-1/3 pl-16'>
+                        <h1 className='text-5xl font-bold'>Affordable Price For Car Servicing</h1>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className='flex items-center gap-6'>
+                            <button className="btn btn-outline btn-secondary">Discover More</button>
+                            <button className="btn btn-active btn-primary">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex gap-4 justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide5" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './Router/Router';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
+)
